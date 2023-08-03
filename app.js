8adc9897679f083a7faf37fcc9f7baeeefd51913
yaminhossen <streatdot616@gmail.com>
2023-08-03 23:34:26 +0600

    frontend setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const server = express();
+const port = 5000;
+
+
+server.set("view engine", "ejs")
+server.set("views", "./views")
+server.use(express.static('public'))
+
+server.get('/', (req, res) =>{
+    res.render('frontend/index.ejs');
+})
+
+
+server.listen(port, ()=>{
+    console.log(`server is running on http://localhost:${port}`)
+})
